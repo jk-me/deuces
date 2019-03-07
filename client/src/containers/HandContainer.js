3cbc043b2d9346fbd9d7450bf0d0b9fedf507b8b
Jenny Kam <jkam17@gmail.com>
2019-03-07 18:36:01 -0500

    change card click function to add and remove cards to selected, remove react logo file

--- client/src/containers/HandContainer.js
+++ client/src/containers/HandContainer.js
@@ -9,7 +9,13 @@ class HandContainer extends React.Component{
 
   cardClick = (card) =>{
     console.log(`clicked! ${card.code}`)
-    this.setState({selected:[...this.state.selected, card.code]})
+    if (this.state.selected.includes(card.code)){
+      this.setState({selected:[...this.state.selected.filter(c => c !== card.code)]})
+    }
+    else {
+      this.setState({selected:[...this.state.selected, card.code]})
+      
+    }
   }
 
   renderHand = (hnum) =>{
@@ -20,7 +26,7 @@ class HandContainer extends React.Component{
     return(
       <div>
           {this.renderHand(this.props.hand)}
-          <p>{this.state.selected}</p>
+          <p>{this.state.selected.join(',')}</p>
           <PlayButton />
       </div>
     )
