30a758c61f37d4cd238831da72e91217a2a58dad
Jenny Kam <jkam17@gmail.com>
2019-03-07 15:11:33 -0500

    add redux, react-redux, redux-thunk packages, set up global store w/ thunk middleware and sample reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import gameReducer from './reducers/game_reducer'
+
+const store = createStore(gameReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ client/src/reducers/game_reducer.js
@@ -0,0 +1,8 @@
+export default function gameReducer(state, action){
+  switch (action.type){
+    case 'LOAD':
+      return state
+    default:
+      return state
+  }
+}
