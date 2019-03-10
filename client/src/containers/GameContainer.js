ebbee516895362606734da499f73615da1576a5c
Jenny Kam <jkam17@gmail.com>
2019-03-09 20:13:48 -0500

    add button to set first player in game, add function to determine first player and reducer dispatch action

--- client/src/containers/GameContainer.js
+++ client/src/containers/GameContainer.js
@@ -12,10 +12,29 @@ class GameContainer extends React.Component{
     // this.props.fetchHand(2)
   }
 
+  determineFirst = () =>{
+    let nums = ['3','4','5','6','7','8','9','10','JACK','QUEEN','KING','ACE','2']
+    let suits = ['DIAMONDS', 'CLUBS', 'HEARTS', 'SPADES']
+    let h1 = this.props.hand1.sort((a,b) => nums.indexOf(a.value) - nums.indexOf(b.value))
+    let h2 = this.props.hand2.sort((a,b) => nums.indexOf(a.value) - nums.indexOf(b.value))
+    if (h1[0].value < h2[0].value)
+      { this.props.setFirst('hand1') } //return 'hand2'
+    else if (h2[0].value < h1[0].value)
+      { this.props.setFirst('hand2')  }
+    else {
+      h1 = h1.filter( el => el.value === h1[0].value).sort((a,b) => suits.indexOf(a.suit) - suits.indexOf(b.suit))
+      h2 = h2.filter( el => el.value === h2[0].value).sort((a,b) => suits.indexOf(a.suit) - suits.indexOf(b.suit))
+      if (suits.indexOf(h1[0].suit) < suits.indexOf(h2[0].suit))
+        { this.props.setFirst('hand1') } //return 'hand2'
+      else if (suits.indexOf(h2[0].suit) < suits.indexOf(h1[0].suit))
+        { this.props.setFirst('hand2')  }
+    }
+  }
+
   newGame = () =>{
     this.props.shuffle()
     this.props.fetchHand(1)
-    this.props.fetchHand(2)
+    this.props.fetchHand(2).then(console.log(this.props.hand1))
   }
 
   render(){
@@ -38,8 +57,9 @@ class GameContainer extends React.Component{
           playTurn= {this.props.playTurn}
           last_played= {this.props.last_played}
         />
-        <p> Your turn: {this.props.player}</p>
+        <p> Your turn: {this.props.player} </p>
         <button onClick={this.newGame}>New Game</button>
+        <button onClick={this.determineFirst}>Set first player</button>
       </div>
     )
   }
@@ -59,6 +79,10 @@ const mapDispatchToProps = dispatch =>{
   return {
     fetchHand: (num) => dispatch(fetchHand(num)),
     shuffle: () => dispatch(shuffleDeck()),
+    setFirst: (player) => dispatch({
+      type: 'SET_FIRST_PLAYER',
+      player: player
+    }),
     playTurn: (selected, player, play) => dispatch({
       type: 'PLAY_TURN',
       selected: selected,
--- client/src/reducers/game_reducer.js
+++ client/src/reducers/game_reducer.js
@@ -3,7 +3,7 @@ export default function gameReducer(
   state = {
     hand1: [],
     hand2:[],
-    player: 'hand1',
+    player: '',
     last_played: {play: '', cards: []}
   },
   action){
@@ -12,6 +12,8 @@ export default function gameReducer(
   switch (action.type){
     case 'DRAW_HAND':
       return {...state, [action.num]: action.cards}
+    case 'SET_FIRST_PLAYER':
+      return {...state, player: action.player}
     case 'PLAY_TURN':
       const next_player = action.player === 'hand1' ? 'hand2' : 'hand1'
       console.log(next_player)
