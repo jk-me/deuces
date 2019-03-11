ce5f5099019ffabc5c48e34aaa13bd8e5ccfe40a
Jenny Kam <jkam17@gmail.com>
2019-03-11 13:50:10 -0400

    connect session info component and display previous game records in a table

--- client/src/components/SessionInfo.js
+++ client/src/components/SessionInfo.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-const SessionInfo = props =>{
-  return(
-    <div>
-      Previous Game Data
-    </div>
-  )
+class SessionInfo extends React.Component{
+
+  renderTableRows = () =>{
+    return this.props.sessions.map(s =>{
+      return (
+        <tr>
+          <th>{s.id}</th>
+          <th>{s.hand1*1}</th>
+          <th>{s.hand2*1}</th>
+          <th>{s.hand1 + s.hand2}</th>
+        </tr>
+      )
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Previous Game Data</h2>
+        <table>
+          <thead>
+          <tr>
+            <th>Session</th>
+            <th>Player 1 wins</th>
+            <th>Player 2 wins</th>
+            <th>Total games</th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.renderTableRows()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
-export default SessionInfo
+const mapStateToProps = state =>{
+  return{
+    sessions: state.sessions
+  }
+}
+
+export default connect(mapStateToProps)(SessionInfo)
