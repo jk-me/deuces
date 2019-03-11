90c40e15d72fbc3e2e75c3f4d3c56b0a3ae56416
Jenny Kam <jkam17@gmail.com>
2019-03-10 22:16:33 -0400

    add react bootstrap, add routes to about, previous game data

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import GameContainer from './containers/GameContainer'
+import About from './components/About'
+import SessionInfo from './components/SessionInfo'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
 
-  // componentDidMount(){
-  // }
-
   render() {
-    // console.log(this.state)
-    // return (
-    //     <>
-    //       <GameContainer/>
-    //
-    //     </>
-    // );
     return (
       <Router>
         <>
-          <Route path='/' render={() => <GameContainer/>} />
-
+          <Route exact path='/' render={() => <GameContainer/>} />
+          <Route exact path='/about' render={() =><About/>} />
+          <Route exact path='/game_data' render={() =><SessionInfo/>} />
         </>
       </Router>
     );
   }
 }
-// <Route exact path='/about' render{() =><About/>} />
-// <Route path='/session_info' render{() =><SessionInfo/>} />
+
 
 export default App;
---
+++ client/src/components/SessionInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SessionInfo = props =>{
+  return(
+    <div>
+      Previous Game Data
+    </div>
+  )
+}
+
+export default SessionInfo
