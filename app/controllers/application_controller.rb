c063e99fd3fc9ab023bb233899807c9195db2a47
Jenny Kam <jkam17@gmail.com>
2019-05-17 17:33:05 -0400

    add fallback routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :games, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+  
 end
