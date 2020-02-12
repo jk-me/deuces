938a4bda7d0685743a1d9f48c507fc6cbfe42e75
Jenny Kam <jkam17@gmail.com>
2020-02-12 12:53:30 -0500

    connect messagebox component to redux, show current player in component

--- client/src/components/MessageBox.js
+++ client/src/components/MessageBox.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class MessageBox  extends React.Component{
-  click = () =>{
-    this.props.clickFn(this.props.card)
+
+  renderMessage = () =>{
+    if (this.props.winner){
+
+    }
   }
+
+  showCurrPlayer = () =>{
+    const {curr_player} = this.props
+    if (curr_player){
+      return(<p>{curr_player} turn</p>)
+    }
+  }
+
   render(){
     return (
       <div class="message">
+        {this.showCurrPlayer()}
         Invalid Hand!
         Player 1 wins!
       </div>
-
     )
   }
 }
 
-export default MessageBox
+const mapStateToProps = state =>{
+  return{
+    winner: state.winner,
+    curr_player: state.player
+  }
+}
+
+
+export default connect(mapStateToProps, null)(MessageBox)
