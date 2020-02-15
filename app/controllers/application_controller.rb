54a9a14e8c57df747d091c5c556526bb56a623bd
Jenny Kam <jkam17@gmail.com>
2020-02-15 17:42:00 -0500

    change fallback_index_html file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def fallback_index_html
-    render :file => 'public/index.html'
+    render :file => 'client/public/index.html'
   end
-  
+
 end
